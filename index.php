7ba7f046447e708f449502ddb216011c3773b132
hisham-cmd <61764346+hisham-cmd@users.noreply.github.com>
2026-08-27 18:15:45 +0300

    Add exception catcher in index.php

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <?php
 
 /**
- * Laravel - A PHP Framework For Web Artisans
- * Router for PHP Built-in Web Server / Wasmer Edge
+ * Laravel Router for Wasmer Edge
  */
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?? ''
 );
 
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri) && !is_dir(__DIR__.'/public'.$uri)) {
     return false;
 }
 
-require_once __DIR__.'/public/index.php';
+try {
+    require_once __DIR__.'/public/index.php';
+} catch (\Throwable $e) {
+    http_response_code(200);
+    header('Content-Type: text/plain; charset=utf-8');
+    echo "Caught Exception on Wasmer Edge:\n";
+    echo "Message: " . $e->getMessage() . "\n";
+    echo "File: " . $e->getFile() . " (Line: " . $e->getLine() . ")\n\n";
+    echo "Trace:\n" . $e->getTraceAsString();
+}
