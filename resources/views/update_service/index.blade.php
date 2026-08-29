acd87b4d601cfa347d1440091ea83d009315b3ab
hisham-cmd <61764346+hisham-cmd@users.noreply.github.com>
2026-08-29 12:33:28 +0300

    feat: add index page for service updates with custom hero and navigation components

--- resources/views/update_service/index.blade.php
+++ resources/views/update_service/index.blade.php
@@ -2758,7 +2758,7 @@ body{display:block!important;background:transparent!important}
 .cards-grid .card-arr{font-size:14px!important;color:#006C35!important}
 
 /* Offices */
-.offices-sec{padding:16px 40px 12px!important;position:relative!important;overflow:hidden!important;background:rgba(255,255,255,.75)!important;border-radius:0 0 16px 16px!important;max-width:1400px!important;    margin: 1px auto !important;}
+.offices-sec{padding:16px 40px 12px!important;position:relative!important;overflow:hidden!important;background:rgba(255,255,255,.75)!important;border-radius:0 0 16px 16px!important;max-width:1400px!important;    margin: 0px auto !important;}
 .offices-sec::before,.offices-sec::after{display:none!important}
 .offices-inner{max-width:1200px!important;margin:0 auto!important;text-align:center!important}
 .s-ttl{font-size:clamp(.7rem,1.4vw,.85rem)!important;font-weight:800!important;color:#002A15!important;margin-bottom:4px!important;text-align:center!important}
